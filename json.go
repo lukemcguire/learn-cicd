dd0877137c03da16cb43f0bee738973efd5733f6
Luke McGuire <luke.mcguire@gmail.com>
2025-05-16 13:16:11 -0700

    fix: cleanup respondWithJSON

--- json.go
+++ json.go
@@ -32,7 +32,6 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.WriteHeader(code)
 	_, err = w.Write(dat)
 	if err != nil {
-		log.Printf("Error writing data: %s", err)
-		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("Critical error writing response: %s", err)
 	}
 }
